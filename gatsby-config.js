aeb306e104ad26365d9e5d509c9f5e3241422126
Ricardo Ruiz <gaanzz11@gmail.com>
2020-08-15 00:12:41 -0500

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: `Firgun`,
+    description: `A documentation site about web dev.`,
+    author: `@Ricardoentuiter`,
+    keywords: ["Web", "Development"],
+    siteUrl: "https://www.firgun.me",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `data`,
+    //     path: `${__dirname}/src/data/`,
+    //     ignore: [`**/\.*`], // ignore files starting with a dot
+    //   },
+    // },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    `gatsby-plugin-mdx`,
+  ],
+}
