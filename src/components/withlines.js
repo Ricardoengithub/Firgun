b0a54bb00ac0fec932eb8c2b6307f95d04687b46
Ricardo Ruiz <gaanzz11@gmail.com>
2020-08-15 23:25:09 -0500

    Css p

--- src/components/withlines.js
+++ src/components/withlines.js
@@ -3,6 +3,23 @@ import styled from "styled-components";
 import Highlight, { defaultProps } from "prism-react-renderer";
 import theme from "prism-react-renderer/themes/nightOwl";
 
+const eCode = `
+import React, { useState } from "react";
+
+function Example() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+`.trim();
+
 const Pre = styled.pre`
   text-align: left;
   margin: 1em 0;
@@ -27,7 +44,7 @@ const LineContent = styled.span`
 `;
 
 const WithLineNumbers = ({exampleCode, lang}) => (
-  <Highlight {...defaultProps} theme={theme} code={exampleCode} language={lang}>
+  <Highlight {...defaultProps} theme={theme} code={exampleCode !== null ? exampleCode : eCode } language={lang}>
     {({ className, style, tokens, getLineProps, getTokenProps }) => (
       <Pre className={className} style={style}>
         {tokens.map((line, i) => (
--- src/pages/css/pcodes.js
+++
@@ -1,7 +0,0 @@
-export const pColor = `p{
-    color: purple;
-}`
-
-export const pFontSize = `p{
-    font-size: 20px;
-}`
