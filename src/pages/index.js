cc2e40fb7b9a403cb88a5251f17619ddb2493cba
Ricardo Ruiz <gaanzz11@gmail.com>
2020-08-17 14:44:39 -0500

    Home page changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ function Home({data}) {
           Firgun es un término hebréo el cuál describe un placer y orgullo por el logro de otra persona, Otra definición describre a Firgun como la generosidad de espíritu y empatía por que algo que le pasó o le pasará a otra persona. 
           </p>
         </Alert>
-        <Row style={{maxWidth: `90vw`, margin: `0 auto`}}>
+        <Row style={{maxWidth: `90vw`, margin: `4rem auto`}}>
         <Card style={{ width: '18rem', margin: `0 auto` }}>
           <Card.Body>
             <Card.Title className="center"><FaCss3Alt style={{fontSize: 100, width: `50%`, margin: `0 4rem` }}/></Card.Title>
@@ -23,8 +23,10 @@ function Home({data}) {
             <Card.Text>
               CSS es un lenguaje de diseño gráfico para definir y crear la presentación de un documento estructurado escrito en un lenguaje de marcado.
             </Card.Text>
-            <Card.Link href="#"><Link to="/css">Docs</Link></Card.Link>
-            <Card.Link href="#"><Link to="/blog/css">Blog</Link></Card.Link>
+            <Row style={{width: `50%`, display: `flex`, justifyContent: `space-around`}}>              
+            <Link to="/css">Docs</Link>
+            <Link to="/blog/css">Blog</Link>
+            </Row>
           </Card.Body>
         </Card>
         <Card style={{ width: '18rem', margin: `0 auto` }}>
@@ -34,8 +36,10 @@ function Home({data}) {
             <Card.Text>
             HTML, que significa Lenguaje de Marcado de Hipertextos (HyperText Markup Language), es la pieza más básica para la construcción de la web y se usa para definir el sentido y estructura del contenido en una página web.
             </Card.Text>
-            <Card.Link href="#"><Link to="/html">Docs</Link></Card.Link>
-            <Card.Link href="#"><Link to="/blog/html">Blog</Link></Card.Link>
+            <Row style={{width: `50%`, display: `flex`, justifyContent: `space-around`}}>              
+            <Link to="/html">Docs</Link>
+            <Link to="/blog/html">Blog</Link>
+            </Row>
           </Card.Body>
         </Card>
         <Card style={{ width: '18rem', margin: `0 auto` }}>
@@ -45,8 +49,10 @@ function Home({data}) {
             <Card.Text>
             JavaScript (JS) es un lenguaje de programación ligero, interpretado, o compilado justo-a-tiempo (just-in-time) con funciones de primera clase. 
             </Card.Text>
-            <Card.Link href="#"><Link to="/javascript">Docs</Link></Card.Link>
-            <Card.Link href="#"><Link to="/blog/javascript">Blog</Link></Card.Link>
+            <Row style={{width: `50%`, display: `flex`, justifyContent: `space-around`}}>              
+            <Link to="/javascript">Docs</Link>
+            <Link to="/blog/javascript">Blog</Link>
+            </Row>
           </Card.Body>
         </Card>
         <Card style={{ width: '18rem', margin: `0 auto` }}>
@@ -56,7 +62,9 @@ function Home({data}) {
             <Card.Text>
             Revisa nuestro blog dónde encontrarás cosas curiosas sobre lenguajes de programación.
             </Card.Text>
-            <Card.Link href="#"><Link to="/blog/javascript">Blog</Link></Card.Link>
+            <Row style={{width: `90%`, margin: `0 auto`}}>
+            <Link to="/blog/javascript">Blog</Link>
+            </Row>
           </Card.Body>
         </Card>
         </Row>
