b0554bf047a425b28329f7dbb1df05ef5df248b5
Ricardo Ruiz <gaanzz11@gmail.com>
2020-08-18 13:33:13 -0500

    ALgolia added

---
+++ src/utils/algolia-queries.js
@@ -0,0 +1,34 @@
+const escapeStringRegexp = require("escape-string-regexp")
+const pagePath = `data`
+const indexName = `Pages`
+const pageQuery = `{
+    pages: allMdx(filter: {fileAbsolutePath: {regex: "/${escapeStringRegexp(pagePath)}/"}}) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+          }
+          slug
+          excerpt(pruneLength: 5000)
+        }
+      }
+    }
+  }`
+function pageToAlgoliaRecord({ node: { id, frontmatter, fields, ...rest } }) {
+  return {
+    objectID: id,
+    ...frontmatter,
+    ...fields,
+    ...rest,
+  }
+}
+const queries = [
+  {
+    query: pageQuery,
+    transformer: ({ data }) => data.pages.edges.map(pageToAlgoliaRecord),
+    indexName,
+    settings: { attributesToSnippet: [`excerpt:20`] },
+  },
+]
+module.exports = queries
